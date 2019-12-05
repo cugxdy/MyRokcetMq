2327c1d2bf72aedb9e34ae724904ee18c28952bb
cugxdy <cugxdy@163.com>
2019-12-05 13:32:18 +0800

    它继承实现了java.util.consurrent.ThreadPoolExecutor对象

--- broker/src/main/java/org/apache/rocketmq/broker/latency/BrokerFixedThreadPoolExecutor.java
+++ broker/src/main/java/org/apache/rocketmq/broker/latency/BrokerFixedThreadPoolExecutor.java
@@ -25,24 +25,29 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class BrokerFixedThreadPoolExecutor extends ThreadPoolExecutor {
+	
+	// 创建BrokerFixedThreadPoolExecutor对象
     public BrokerFixedThreadPoolExecutor(final int corePoolSize, final int maximumPoolSize, final long keepAliveTime,
         final TimeUnit unit,
         final BlockingQueue<Runnable> workQueue) {
         super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
     }
 
+    // 创建BrokerFixedThreadPoolExecutor对象, 线程工厂threadFactory
     public BrokerFixedThreadPoolExecutor(final int corePoolSize, final int maximumPoolSize, final long keepAliveTime,
         final TimeUnit unit,
         final BlockingQueue<Runnable> workQueue, final ThreadFactory threadFactory) {
         super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory);
     }
 
+    // 创建BrokerFixedThreadPoolExecutor对象,拒绝策略RejectedExecutionHandler
     public BrokerFixedThreadPoolExecutor(final int corePoolSize, final int maximumPoolSize, final long keepAliveTime,
         final TimeUnit unit,
         final BlockingQueue<Runnable> workQueue, final RejectedExecutionHandler handler) {
         super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, handler);
     }
 
+    // 创建BrokerFixedThreadPoolExecutor,线程工厂threadFactory,拒绝策略RejectedExecutionHandler
     public BrokerFixedThreadPoolExecutor(final int corePoolSize, final int maximumPoolSize, final long keepAliveTime,
         final TimeUnit unit,
         final BlockingQueue<Runnable> workQueue, final ThreadFactory threadFactory,
@@ -50,7 +55,7 @@ public class BrokerFixedThreadPoolExecutor extends ThreadPoolExecutor {
         super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, handler);
     }
 
-    @Override
+    @Override // RunnableFuture对象
     protected <T> RunnableFuture<T> newTaskFor(final Runnable runnable, final T value) {
         return new FutureTaskExt<T>(runnable, value);
     }
