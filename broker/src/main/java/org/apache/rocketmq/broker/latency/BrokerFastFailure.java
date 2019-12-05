cc385fc6a5e3b25a2df145b9367ef30b9829c0dc
cugxdy <cugxdy@163.com>
2019-12-05 14:09:24 +0800

    它是用于在系统繁忙时, 直接向客户端写入SYSTEM_BUSY标识符, 并定时去清除运行过期的RequestTask对象

--- broker/src/main/java/org/apache/rocketmq/broker/latency/BrokerFastFailure.java
+++ broker/src/main/java/org/apache/rocketmq/broker/latency/BrokerFastFailure.java
@@ -33,18 +33,25 @@ import org.slf4j.LoggerFactory;
  * {@link BrokerController#pullThreadPoolQueue}
  */
 public class BrokerFastFailure {
+	
     private static final Logger log = LoggerFactory.getLogger(LoggerName.BROKER_LOGGER_NAME);
+    
+    // 定时任务线程池
     private final ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor(new ThreadFactoryImpl(
         "BrokerFastFailureScheduledThread"));
+    
+    // Broker启动控制器
     private final BrokerController brokerController;
 
+    
     public BrokerFastFailure(final BrokerController brokerController) {
         this.brokerController = brokerController;
     }
 
+    // 获取RequestTask对象
     public static RequestTask castRunnable(final Runnable runnable) {
         try {
-            FutureTaskExt object = (FutureTaskExt) runnable;
+            FutureTaskExt<?> object = (FutureTaskExt<?>) runnable;
             return (RequestTask) object.getRunnable();
         } catch (Throwable e) {
             log.error(String.format("castRunnable exception, %s", runnable.getClass().getName()), e);
@@ -53,10 +60,13 @@ public class BrokerFastFailure {
         return null;
     }
 
+    // Broker启动选项
     public void start() {
+    	// 定时任务, 1s启动, 10ms周期
         this.scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
             @Override
             public void run() {
+            	// default = true
                 if (brokerController.getBrokerConfig().isBrokerFastFailureEnable()) {
                     cleanExpiredRequest();
                 }
@@ -65,16 +75,27 @@ public class BrokerFastFailure {
     }
 
     private void cleanExpiredRequest() {
+    	// 当前系统处于繁忙状态, lockDiff > 1000ms
         while (this.brokerController.getMessageStore().isOSPageCacheBusy()) {
             try {
+            	
+            	// 判断发送线程池任务队列是否为空
                 if (!this.brokerController.getSendThreadPoolQueue().isEmpty()) {
+                	// 获取线程池任务
                     final Runnable runnable = this.brokerController.getSendThreadPoolQueue().poll(0, TimeUnit.SECONDS);
                     if (null == runnable) {
                         break;
                     }
 
+                    // 获取RequestTask对象
                     final RequestTask rt = castRunnable(runnable);
-                    rt.returnResponse(RemotingSysResponseCode.SYSTEM_BUSY, String.format("[PCBUSY_CLEAN_QUEUE]broker busy, start flow control for a while, period in queue: %sms, size of queue: %d", System.currentTimeMillis() - rt.getCreateTimestamp(), this.brokerController.getSendThreadPoolQueue().size()));
+                    
+                    // 设置系统繁忙, 向客户端返回系统繁忙, 暂时不支持消息Pull拉取
+                    rt.returnResponse(RemotingSysResponseCode.SYSTEM_BUSY, 
+                    		String.format("[PCBUSY_CLEAN_QUEUE]broker busy, start flow control for a while, "
+                    		+ "period in queue: %sms, size of queue: %d", 
+                    		System.currentTimeMillis() - rt.getCreateTimestamp(), 
+                    		this.brokerController.getSendThreadPoolQueue().size()));
                 } else {
                     break;
                 }
@@ -92,20 +113,27 @@ public class BrokerFastFailure {
     void cleanExpiredRequestInQueue(final BlockingQueue<Runnable> blockingQueue, final long maxWaitTimeMillsInQueue) {
         while (true) {
             try {
+            	// 判断阻塞队列是否为空
                 if (!blockingQueue.isEmpty()) {
+                	// 从blockingQueue中获取任务队列但并删除任务
                     final Runnable runnable = blockingQueue.peek();
                     if (null == runnable) {
                         break;
                     }
+                    
+                    // 获取RequestTask任务
                     final RequestTask rt = castRunnable(runnable);
+                    // RequestTask运行任务是否停止
                     if (rt == null || rt.isStopRun()) {
                         break;
                     }
 
                     final long behind = System.currentTimeMillis() - rt.getCreateTimestamp();
+                    // 判断是否达到最大时间
                     if (behind >= maxWaitTimeMillsInQueue) {
                         if (blockingQueue.remove(runnable)) {
                             rt.setStopRun(true);
+                            // 设置系统繁忙
                             rt.returnResponse(RemotingSysResponseCode.SYSTEM_BUSY, String.format("[TIMEOUT_CLEAN_QUEUE]broker busy, start flow control for a while, period in queue: %sms, size of queue: %d", behind, blockingQueue.size()));
                         }
                     } else {
@@ -119,6 +147,7 @@ public class BrokerFastFailure {
         }
     }
 
+    // shutdown 定时任务线程池
     public void shutdown() {
         this.scheduledExecutorService.shutdown();
     }
