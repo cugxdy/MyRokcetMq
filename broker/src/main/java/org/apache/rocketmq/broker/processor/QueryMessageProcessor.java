97a80f9e311fba4fd0118ec8a134b2c55d419e1f
cugxdy <cugxdy@163.com>
2019-12-06 10:52:18 +0800

    从Broker服务器上使用IndexFile对象去查询Message数据对象

--- broker/src/main/java/org/apache/rocketmq/broker/processor/QueryMessageProcessor.java
+++ broker/src/main/java/org/apache/rocketmq/broker/processor/QueryMessageProcessor.java
@@ -41,6 +41,7 @@ import org.slf4j.LoggerFactory;
 public class QueryMessageProcessor implements NettyRequestProcessor {
     private static final Logger log = LoggerFactory.getLogger(LoggerName.BROKER_LOGGER_NAME);
 
+    // Broker服务控制器对象
     private final BrokerController brokerController;
 
     public QueryMessageProcessor(final BrokerController brokerController) {
@@ -50,10 +51,13 @@ public class QueryMessageProcessor implements NettyRequestProcessor {
     @Override
     public RemotingCommand processRequest(ChannelHandlerContext ctx, RemotingCommand request)
         throws RemotingCommandException {
-        switch (request.getCode()) {
+        
+    	switch (request.getCode()) {
             case RequestCode.QUERY_MESSAGE:
+            	// 使用IndexFile对象查询消息,并返回结果对象
                 return this.queryMessage(ctx, request);
             case RequestCode.VIEW_MESSAGE_BY_ID:
+            	// 向Broker服务器查询一个消息
                 return this.viewMessageById(ctx, request);
             default:
                 break;
@@ -69,28 +73,40 @@ public class QueryMessageProcessor implements NettyRequestProcessor {
 
     public RemotingCommand queryMessage(ChannelHandlerContext ctx, RemotingCommand request)
         throws RemotingCommandException {
+    	
+    	// 获取响应报文数据
         final RemotingCommand response =
             RemotingCommand.createResponseCommand(QueryMessageResponseHeader.class);
         final QueryMessageResponseHeader responseHeader =
             (QueryMessageResponseHeader) response.readCustomHeader();
+        
+        // 获取查询消息请求报文数据
         final QueryMessageRequestHeader requestHeader =
             (QueryMessageRequestHeader) request
                 .decodeCommandCustomHeader(QueryMessageRequestHeader.class);
 
         response.setOpaque(request.getOpaque());
 
+        // 从Map中获取_UNIQUE_KEY_QUERY属性值
         String isUniqueKey = request.getExtFields().get(MixAll.UNIQUE_MSG_QUERY_FLAG);
+        
+        // true : 消息唯一Key == "true"
         if (isUniqueKey != null && isUniqueKey.equals("true")) {
+        	// default = 32;
             requestHeader.setMaxNum(this.brokerController.getMessageStoreConfig().getDefaultQueryMaxNum());
         }
 
+        // 使用IndexFile从commitLog中去查询指定数目的消息数据报文
         final QueryMessageResult queryMessageResult =
             this.brokerController.getMessageStore().queryMessage(requestHeader.getTopic(),
                 requestHeader.getKey(), requestHeader.getMaxNum(), requestHeader.getBeginTimestamp(),
                 requestHeader.getEndTimestamp());
         assert queryMessageResult != null;
-
+        
+        // 设置IndexService最新最大物理偏移量(lastIndexFile);
         responseHeader.setIndexLastUpdatePhyoffset(queryMessageResult.getIndexLastUpdatePhyoffset());
+        
+        // 设置IndexService最新更新时间戳(lastIndexFile);
         responseHeader.setIndexLastUpdateTimestamp(queryMessageResult.getIndexLastUpdateTimestamp());
 
         if (queryMessageResult.getBufferTotalSize() > 0) {
@@ -98,9 +114,12 @@ public class QueryMessageProcessor implements NettyRequestProcessor {
             response.setRemark(null);
 
             try {
+            	// 将消息封装成FileRegion对象
                 FileRegion fileRegion =
                     new QueryMessageTransfer(response.encodeHeader(queryMessageResult
                         .getBufferTotalSize()), queryMessageResult);
+                
+                // 写入Channel对象
                 ctx.channel().writeAndFlush(fileRegion).addListener(new ChannelFutureListener() {
                     @Override
                     public void operationComplete(ChannelFuture future) throws Exception {
@@ -118,6 +137,7 @@ public class QueryMessageProcessor implements NettyRequestProcessor {
             return null;
         }
 
+        // 设置为消息找不到状态
         response.setCode(ResponseCode.QUERY_NOT_FOUND);
         response.setRemark("can not find message, maybe time range not correct");
         return response;
@@ -126,21 +146,28 @@ public class QueryMessageProcessor implements NettyRequestProcessor {
     public RemotingCommand viewMessageById(ChannelHandlerContext ctx, RemotingCommand request)
         throws RemotingCommandException {
         final RemotingCommand response = RemotingCommand.createResponseCommand(null);
+        
+        // 获取网络请求报文数据ViewMessageRequestHeader对象
         final ViewMessageRequestHeader requestHeader =
             (ViewMessageRequestHeader) request.decodeCommandCustomHeader(ViewMessageRequestHeader.class);
 
         response.setOpaque(request.getOpaque());
 
+        // 获取消息ByteBuffer数据
         final SelectMappedBufferResult selectMappedBufferResult =
             this.brokerController.getMessageStore().selectOneMessageByOffset(requestHeader.getOffset());
+        
         if (selectMappedBufferResult != null) {
             response.setCode(ResponseCode.SUCCESS);
             response.setRemark(null);
 
             try {
+            	// 将其封装成FileRegion对象
                 FileRegion fileRegion =
                     new OneMessageTransfer(response.encodeHeader(selectMappedBufferResult.getSize()),
                         selectMappedBufferResult);
+                
+                // 将FileRegion对象写入Channel对象中去
                 ctx.channel().writeAndFlush(fileRegion).addListener(new ChannelFutureListener() {
                     @Override
                     public void operationComplete(ChannelFuture future) throws Exception {
@@ -157,6 +184,7 @@ public class QueryMessageProcessor implements NettyRequestProcessor {
 
             return null;
         } else {
+        	// 设置为系统错误
             response.setCode(ResponseCode.SYSTEM_ERROR);
             response.setRemark("can not find message by the offset, " + requestHeader.getOffset());
         }
