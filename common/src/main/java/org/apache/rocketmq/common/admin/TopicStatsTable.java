6d0d45ccca41615fe8530a2d8d8a700521f9e921
cugxdy <cugxdy@163.com>
2019-12-12 11:21:23 +0800

    它是记录topic下的ConsumerQueue相关信息(MinOffset, MaxOffset)

--- common/src/main/java/org/apache/rocketmq/common/admin/TopicStatsTable.java
+++ common/src/main/java/org/apache/rocketmq/common/admin/TopicStatsTable.java
@@ -20,6 +20,7 @@ import java.util.HashMap;
 import org.apache.rocketmq.common.message.MessageQueue;
 import org.apache.rocketmq.remoting.protocol.RemotingSerializable;
 
+// 它是记录topic下的ConsumerQueue相关信息(MinOffset, MaxOffset) 
 public class TopicStatsTable extends RemotingSerializable {
     private HashMap<MessageQueue, TopicOffset> offsetTable = new HashMap<MessageQueue, TopicOffset>();
 
