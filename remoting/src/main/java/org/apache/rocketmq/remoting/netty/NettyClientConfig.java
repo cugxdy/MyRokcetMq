d0da3ac405e1bfc212ce0e607de4136af0199bc9
cugxdy <cugxdy@163.com>
2019-12-13 13:43:52 +0800

    它netty服务中客户端配置对象

--- remoting/src/main/java/org/apache/rocketmq/remoting/netty/NettyClientConfig.java
+++ remoting/src/main/java/org/apache/rocketmq/remoting/netty/NettyClientConfig.java
@@ -20,22 +20,36 @@ public class NettyClientConfig {
     /**
      * Worker thread number
      */
+	// 它是记录处理inbound、outbound处理流线程数
     private int clientWorkerThreads = 4;
+    
+    // 执行invokeCall回调线程组个数, publicExecutor线程池数量
     private int clientCallbackExecutorThreads = Runtime.getRuntime().availableProcessors();
+    
+    // 网络通信oneway方式: 令牌数
     private int clientOnewaySemaphoreValue = NettySystemConfig.CLIENT_ONEWAY_SEMAPHORE_VALUE;
+    // 网络通信async方式: 令牌数
     private int clientAsyncSemaphoreValue = NettySystemConfig.CLIENT_ASYNC_SEMAPHORE_VALUE;
+    
+    // 向服务器发起Tcp连接请求超时时间戳=3000;
     private int connectTimeoutMillis = 3000;
+    
     private long channelNotActiveInterval = 1000 * 60;
 
     /**
      * IdleStateEvent will be triggered when neither read nor write was performed for
      * the specified period of this time. Specify {@code 0} to disable
      */
+    // 客户端读写空闲触发时间
     private int clientChannelMaxIdleTimeSeconds = 120;
 
     private int clientSocketSndBufSize = NettySystemConfig.socketSndbufSize;
     private int clientSocketRcvBufSize = NettySystemConfig.socketRcvbufSize;
+    
+    // 是否使用netty提供的池化ByteBuf对象
     private boolean clientPooledByteBufAllocatorEnable = false;
+    
+    // 超时发生时,是否关闭Channel对象
     private boolean clientCloseSocketIfTimeout = false;
 
     // 安全传输层协议(TLS)
